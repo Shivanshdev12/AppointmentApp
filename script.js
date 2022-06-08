962d637e97e0d3ce75d71078a72bf7a194eefe76
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-08 15:52:05 +0530

    displayed from localstorage

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+let myObj;
+
 function storeDetails(e) {
-  localStorage.setItem("Name", e.target.Name.value);
-  localStorage.setItem("Email", e.target.Email.value);
-  localStorage.setItem("Phone", e.target.Phone.value);
-  localStorage.setItem("Date", e.target.date.value);
-  localStorage.setItem("Time", e.target.time.value);
-  console.log(e.target.date.value);
+  myObj = {
+    Name: e.target.Name.value,
+    Email: e.target.Email.value,
+    Phone: e.target.Phone.value,
+    Date: e.target.date.value,
+    Time: e.target.time.value,
+  };
+  localStorage.setItem("myObj", JSON.stringify(myObj));
+  console.log(localStorage);
+  //   display();
 }
+
+function display() {
+  let myObj = JSON.parse(localStorage.getItem("myObj"));
+  let user = new Array();
+  user = Object.values(myObj);
+  var itemList = document.querySelector(".item-list");
+  for (var i = 0; i < user.length; i++) {
+    var li = document.createElement("li");
+    li.className = "list";
+    li.appendChild(document.createTextNode(user[i]));
+    itemList.appendChild(li);
+  }
+}
+
+// myObj = JSON.parse(localStorage.getItem("myObj"));
+// let user = new Array();
+// user = Object.values(myObj);
+// var itemList = document.querySelector(".item-list");
+// for (var i = 0; i < user.length; i++) {
+//   var li = document.createElement("li");
+//   li.className = "list";
+//   li.appendChild(document.createTextNode(user[i]));
+//   itemList.appendChild(li);
+// }
