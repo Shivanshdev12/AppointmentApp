f104eaf27a18335ef47ea0eb7f7f31668dccec01
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-06-09 14:40:13 +0530

    edit/delete added

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-var obj = {};
-window.addEventListener("DOMContentLoaded", (event) => {
-  display();
+window.addEventListener("DOMContentLoaded", (e) => {
+  Object.keys(localStorage).forEach(function (key) {
+    if (key) {
+      const stringifiedList = localStorage.getItem(key);
+      console.log(stringifiedList);
+      const user = JSON.parse(stringifiedList);
+      console.log(user);
+
+      addNewUser(user);
+    }
+  });
 });
 
 function storeDetails(e) {
   e.preventDefault();
-  const name = e.target.Name.value;
-  const Email = e.target.Email.value;
-  const Phone = e.target.Phone.value;
-  const Date = e.target.date.value;
-  const Time = e.target.time.value;
-  obj = {
+  const Email = document.getElementById("email").value;
+  const name = document.getElementById("name").value;
+  const Phone = document.getElementById("phone").value;
+  const obj = {
     name,
-    Email,
     Phone,
-    Date,
-    Time,
+    Email,
   };
   localStorage.setItem(obj.Email, JSON.stringify(obj));
   addNewUser(obj);
 }
 
-function addNewUser(user) {
-  const parentNode = document.getElementById("items");
-  const child = `<li>${user.name}, ${user.Email}, ${user.Phone},
-    ${user.Date}, ${user.Time.toLocaleString()}</li>`;
-  parentNode.innerHTML = parentNode.innerHTML + child;
-}
+function addNewUser(obj) {
+  const ul = document.getElementById("items");
+  const li = document.createElement("li");
 
-function display() {
-  Object.keys(localStorage).forEach((key) => {
-    stringifiedUser = localStorage.getItem(key);
-    console.log(stringifiedUser);
-    user = JSON.parse(stringifiedUser);
-    // console.log(user);
-    const parentNode = document.getElementById("items");
-    const child = `<li>${user.name}, ${user.Email}, ${user.Phone},
-    ${user.Date}, ${user.Time.toLocaleString()}</li>`;
-    parentNode.innerHTML = parentNode.innerHTML + child;
+  li.appendChild(
+    document.createTextNode(obj.name + " " + obj.Email + " " + obj.Phone)
+  );
+  const btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("Edit"));
+  btn.addEventListener("click", function () {
+    document.getElementById("name").value = obj.name;
+    document.getElementById("email").value = obj.Email;
+    document.getElementById("phone").value = obj.Phone;
+    li.remove();
   });
+  li.appendChild(btn);
+
+  deleteBtn = document.createElement("button");
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  deleteBtn.addEventListener("click", function () {
+    localStorage.removeItem(obj.Email);
+    li.remove();
+  });
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
 }
